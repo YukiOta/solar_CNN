3b28af4a16404830a5f673fe7c0c495203fc7a55
YukiOta <yuki.ota@ruri.waseda.jp>
2017-07-25 17:00:52 +0900

    0725

--- Load_data.py
+++ Load_data.py
@@ -8,6 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
+import datetime as dt
 from PIL import Image
 
 
@@ -30,12 +31,42 @@ def load_image(imgdir, size, norm=True):
     return img_array
 
 
-def load_target(csv):
+def load_target(csv, imgdir, interval):
     """
     csvファイルを読み込む
     パンダスを用いて、処理する
+    csv: csvファイルのありか
+    imgdir: 対応するimgdirのありか
+    interval: 撮影インターバル (sec)
     """
-    df = pd.read_csv(csv, header=None)
+    # csvファイルの読み込み
+    df = pd.read_csv(csv)
+
+    # 必要な列を取り出し、必要のない行を取り除く
+    df = df.iloc[:, [0, 11, 36]]
+    df.columns = ["time", "power", "temperature"]
+    df = df.drop([0, 1])
+    # データフレームをnumpy配列にする
+    tmptmp = np.array(df)
+
+    # 画像撮影時刻を補正する
+    filelist = os.listdir(imgdir)
+    time_start, time_end = check_target_time(filelist=filelist)
+
+    #
+    for i in range(len(tmptmp)):
+
+        if tmptmp[i][0].replace(':', '') == time_start:
+            i_start = i
+            # print(i)
+            # print(tmptmp[i])
+        elif tmptmp[i][0].replace(':', '') == time_end:
+            i_end = i
+            # print(i)
+            # print(tmptmp[i])
+    interval = 60
+    ota = tmptmp[i_start:i_end:int(interval/6)]
+
     target = []
     target.append(df.values[:, 0])
     target.append(df.values[:, 1])
@@ -44,31 +75,97 @@ def load_target(csv):
     return target
 
 
+def check_target_time(filelist):
+    """
+    画像の時刻と、ファイルの時刻が一致しないことがあるので、チェックする。
+    """
+    file_list = filelist
+    time_tmp = file_list[0][9:15]
+    if int(time_tmp[-2:]) % 6 >= 3:
+        delta = 6 - (int(time_tmp) % 6)
+        timedelta = dt.timedelta(seconds=delta)
+        time_start = dt.datetime.strptime(time_tmp, "%H%M%S")
+        time_correct = time_start + timedelta
+        time_correct_s = time_correct.strftime("%H%M%S")
+    elif int(time_tmp[-2:]) % 6 < 3:
+        delta = int(time_tmp[-2:]) % 6
+        timedelta = dt.timedelta(seconds=delta)
+        time_start = dt.datetime.strptime(time_tmp, "%H%M%S")
+        time_correct = time_start - timedelta
+        time_correct_s = time_correct.strftime("%H%M%S")
+
+    time_tmp = file_list[-2][9:15]
+    if int(time_tmp[-2:]) % 6 >= 3:
+        delta = 6 - (int(time_tmp) % 6)
+        timedelta = dt.timedelta(seconds=delta)
+        time_start = dt.datetime.strptime(time_tmp, "%H%M%S")
+        time_correct = time_start + timedelta
+        time_correct_e = time_correct.strftime("%H%M%S")
+    elif int(time_tmp[-2:]) % 6 < 3:
+        delta = int(time_tmp[-2:]) % 6
+        timedelta = dt.timedelta(seconds=delta)
+        time_start = dt.datetime.strptime(time_tmp, "%H%M%S")
+        time_correct = time_start - timedelta
+        time_correct_e = time_correct.strftime("%H%M%S")
+
+    return time_correct_s, time_correct_e
+
+
 df = pd.read_csv("/Users/yukiota/solar_project/data/PV_CSV/201611/Trd20161117_1.csv")
-df = df.ix[:, [0, 11, 36]]
+df = df.iloc[:, [0, 11, 36]]
 df.columns = ["time", "power", "temperature"]
-df[0:2]
+df = df.drop([0, 1])
+df[7500:8000]
+file_list = os.listdir("/Users/yukiota/solar_project/data/PV_IMAGE/201611/20161117_IT_resampled_northup")
+file_list[0][9:15]
+file_list[-2][9:15]
+tmptmp = np.array(df)
+
+time_tmp = file_list[0][9:15]
+if int(time_tmp[-2:]) % 6 >= 3:
+    delta = 6 - (int(time_tmp) % 6)
+    timedelta = dt.timedelta(seconds=delta)
+    time_start = dt.datetime.strptime(time_tmp, "%H%M%S")
+    time_correct = time_start + timedelta
+    time_correct_s = time_correct.strftime("%H%M%S")
+elif int(time_tmp[-2:]) % 6 < 3:
+    delta = int(time_tmp[-2:]) % 6
+    timedelta = dt.timedelta(seconds=delta)
+    time_start = dt.datetime.strptime(time_tmp, "%H%M%S")
+    time_correct = time_start - timedelta
+    time_correct_s = time_correct.strftime("%H%M%S")
+
+time_tmp = file_list[-2][9:15]
+if int(time_tmp[-2:]) % 6 >= 3:
+    delta = 6 - (int(time_tmp) % 6)
+    timedelta = dt.timedelta(seconds=delta)
+    time_start = dt.datetime.strptime(time_tmp, "%H%M%S")
+    time_correct = time_start + timedelta
+    time_correct_e = time_correct.strftime("%H%M%S")
+elif int(time_tmp[-2:]) % 6 < 3:
+    delta = int(time_tmp[-2:]) % 6
+    timedelta = dt.timedelta(seconds=delta)
+    time_start = dt.datetime.strptime(time_tmp, "%H%M%S")
+    time_correct = time_start - timedelta
+    time_correct_e = time_correct.strftime("%H%M%S")
 
 
+for i in range(len(tmptmp)):
+
+    if tmptmp[i][0].replace(':', '') == time_correct_s:
+        i_start = i
+        print(i)
+        print(tmptmp[i])
+    elif tmptmp[i][0].replace(':', '') == time_correct_e:
+        i_end = i
+        print(i)
+        print(tmptmp[i])
+interval = 60
+ota = tmptmp[i_start:i_end:int(interval/6)]
+ota.shape
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+len(file_list[0:-2])
 
 
 
--- solar_CNN_keras.py
+++ solar_CNN_keras.py
@@ -15,6 +15,7 @@ import datetime as dt
 import os
 import sys
 import seaborn as sns
+import glob
 matplotlib.use('Agg')
 
 SAVE_dir = "./RESULT/CNN_keras/"
@@ -98,6 +99,55 @@ def CNN_model3(activation="relu", loss="mean_squared_error", optimizer="Adadelta
     return model
 
 
+def data_plot(model, target, img, batch_size=10, date="hoge", save_csv=False):
+
+    num = []
+    time = []
+    for i in range(target[0].shape[0]):
+        if i % 50 == 0:
+            num.append(i)
+            time.append(int(target[0][i]))
+        if i == target[0].shape[0] - 1:
+            num.append(i)
+            time.append(int(target[0][i]))
+    img_ = img.transpose(0, 3, 1, 2).copy()
+    pred = model.predict(img_, batch_size=batch_size, verbose=1).copy()
+    if pred.shape:
+        print(pred.shape)
+    if type(pred):
+        print(type(pred))
+    print(target[1].shape)
+    plt.figure()
+    plt.plot(pred, label="Predicted")
+    plt.plot(target[1], label="Observed")
+    plt.legend(loc='best')
+    plt.title("Prediction tested on"+date)
+    plt.xlabel("Time")
+    plt.ylabel("Generated Power[kW]")
+    plt.ylim(0, 25)
+    plt.xticks(num, time)
+
+    pred_ = pred.reshape(pred.shape[0])
+    if save_csv is True:
+        save_target_and_prediction(target=target, pred=pred_, title=date)
+
+    filename = date + "_data"
+    i = 0
+    while os.path.exists(SAVE_dir+'{}{:d}.png'.format(filename, i)):
+        i += 1
+    plt.savefig(SAVE_dir+'{}{:d}.png'.format(filename, i))
+
+
+def save_target_and_prediction(target, pred, title):
+
+    pred_df = pd.DataFrame(pred)
+    target_df = pd.DataFrame(target)
+    df = pd.concat([target_df, pred_df], axis=1)
+    df.columns = ["TARGET", "PREDICTION"]
+    df.to_csv(SAVE_dir+"Error_csv_"+title+".csv")
+
+
+def main():
 
 
 
@@ -106,17 +156,29 @@ def CNN_model3(activation="relu", loss="mean_squared_error", optimizer="Adadelta
 
 
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_dir",
+        default="../data/",
+        help="choose your data (image) directory"
+    )
+    parser.add_argument(
+        "--target_dir",
+        default="../data/",
+        help="choose your target dir"
+    )
+    args = parser.parse_args()
+    DATA_DIR, TARGET_DIR = args.data_dir, args.target_dir
+    main()
 
 
 
 
 
 
-
-
-
-
-
+os.path.exists(".py")
+glob.glob(os.getcwd()+"/*.py")
 
 
 
